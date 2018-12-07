126da990fd8ac18d5e6a27c8dd92188dac8e204d
1027 <1027@hb6026>
2018-12-07 18:18:57 +0900

    정문호 커밋

---
+++ LastProject/src/main/webapp/views/google.js
@@ -0,0 +1,16 @@
+[
+{"lat":"37.5324201","lng":"126.99034879999999","icon" :"http://maps.google.com/mapfiles/ms/micons/yellow-dot.png","cont":"YongSan"},
+{"lat":"37.5630554","lng":"127.03671250000002","icon" :"http://maps.google.com/mapfiles/ms/micons/yellow-dot.png","cont":"SeongDong"},
+{"lat":"37.2980237","lng":"127.63716279999994","icon" :"http://maps.google.com/mapfiles/ms/micons/yellow-dot.png","cont":"YeoJu"},
+{"lat":"37.558085","lng":"126.984240","icon" :"http://maps.google.com/mapfiles/ms/micons/yellow-dot.png","cont":"Seoul01"},
+{"lat":"37.548200","lng":"126.983523","icon" :"http://maps.google.com/mapfiles/ms/micons/yellow-dot.png","cont":"Seoul02"},
+{"lat":"37.550862","lng":"126.981354","icon" :"http://maps.google.com/mapfiles/ms/micons/yellow-dot.png","cont":"Seoul03"},
+{"lat":"37.558350","lng":"126.985702","icon" :"http://maps.google.com/mapfiles/ms/micons/yellow-dot.png","cont":"Seoul04"},
+{"lat":"37.555726","lng":"126.994304","icon" :"http://maps.google.com/mapfiles/ms/micons/yellow-dot.png","cont":"Seoul05"},
+{"lat":"37.542687","lng":"127.005739","icon" :"http://maps.google.com/mapfiles/ms/micons/yellow-dot.png","cont":"Seoul06"},
+{"lat":"37.539314","lng":"126.997142","icon" :"http://maps.google.com/mapfiles/ms/micons/yellow-dot.png","cont":"Seoul07"},
+{"lat":"37.545269","lng":"127.001498","icon" :"http://maps.google.com/mapfiles/ms/micons/yellow-dot.png","cont":"Seoul08"},
+{"lat":"37.555272","lng":"126.996252","icon" :"http://maps.google.com/mapfiles/ms/micons/yellow-dot.png","cont":"Seoul09"},
+{"lat":"37.558886","lng":"126.986356","icon" :"http://maps.google.com/mapfiles/ms/micons/yellow-dot.png","cont":"Seoul10"},
+{"lat":"37.6659753","lng":"127.27930229999993","icon" :"Gyongi","cont":"GyongGi01"}
+]
